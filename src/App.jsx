daa6ccf6a0fc7570030417f51dc3cfe0445c574d
Sachin-Avhad30 <sachinavhad2003@gmail.com>
2026-01-21 10:23:03 +0530

    images data

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Placement from "./Pages/Placement";
 import Courses from "./Pages/Courses";
 import CampusLife from "./Pages/CampusLife";
 import Faculty from "./Pages/Faculty";
-import OPDC from "./Pages/OPDC"; // ✅ ADD THIS
+import OPDC from "./Pages/OPDC";
 import Footer from "./Components/Footer/Footer";
 import ContactSection from "./Pages/ContactSection";
 
@@ -15,7 +15,6 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-  
         <Route path="/placement" element={<Placement />} />
         <Route path="/courses" element={<Courses />} />
         <Route path="/campus-life" element={<CampusLife />} />
